c38cba0a09dbad97915b3548a442e0549770d0e4
karlo <karlo.stjepanovic3@gmail.com>
2023-12-30 23:19:24 +0100

    Tweaks

--- src/utils/form.ts
+++ src/utils/form.ts
@@ -3,12 +3,14 @@ import FormField from "./formField.ts";
 import StringValidator from "../validation/stringValidator.ts";
 import BaseValidator from "../validation/baseValidator.ts";
 import IntegerValidator from "../validation/integerValidator.ts";
+import { globals } from "./globals.ts";
 
 interface DynamicObject {
     [key: string]: any;
-}
+} 
 
 class Form {
+    public processing: boolean = false;
     private formUrl: string;
     private formMethod: string;
     private readonly fields: string[];
@@ -60,6 +62,16 @@ class Form {
         return result;
     }
 
+    getValues(): DynamicObject {
+        const result: DynamicObject = {};
+        this.fields.forEach((key: string) => {
+            if (this[key] instanceof FormField) {
+                result[key] = this[key].value;
+            }
+        });
+        return result;
+    }
+
     validate(): boolean {
         let isValid = true;
         this.fields.forEach((key: string) => {
@@ -73,13 +85,39 @@ class Form {
         return isValid;
     }
 
-    submit(): void {
-        if (this.validate()) {
-            console.log(this.getFields());
-        } else {
-            console.log("Form validation failed.");
-        }
+    submit(): Promise<DynamicObject> {
+        this.processing = true;
+        return new Promise((resolve, reject) => {
+            if (this.validate()) {
+                const url = globals.host + this.formUrl;
+        
+                fetch(url, {
+                    method: this.formMethod,
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    credentials: 'include',
+                    body: JSON.stringify(this.getValues()),
+                })
+                .then((response) => {
+                    if (response.ok) {
+                        resolve(response.json() as DynamicObject);
+                    } else {
+                        reject(new Error(`Failed to submit form. Status: ${response.status}`));
+                    }
+                })
+                .catch((error) => {
+                    reject(error);
+                }).finally(() => {
+                    this.processing = false;
+                });
+            } else {
+                this.processing = false;
+                reject("Form validation failed.");
+            }
+        });
     }
+      
 }
 
 export default Form;
